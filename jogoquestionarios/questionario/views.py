a8b05adf05d6e5cd9f90e2f695c84d00be0afa48
Vinícius Alves Hax <viniciushax@gmail.com>
2021-09-18 20:45:10 -0300

    Adicionando view básica do projeto

--- jogoquestionarios/questionario/views.py
+++ jogoquestionarios/questionario/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.http import HttpResponse
+from django.template import loader
+from .models import Question
+from django.utils import timezone
 
-# Create your views here.
+def index(request):
+	return HttpResponse("Olá mundo")
